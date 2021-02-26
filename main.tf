e95f031c7d876c3f47a8cc47250940f5e3d1a0a3
jagjot93 <71182607+jagjot93@users.noreply.github.com>
2021-02-27 01:56:27 +0530

    Update role_arn

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   version = "3.18.0"
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::536530634742:role/admin-access"
+    role_arn = "arn:aws:iam::459772773625:role/admin-access"
   }
 }
